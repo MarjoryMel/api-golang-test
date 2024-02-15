4bbf613ed52ac49ab97127f9a4f460f916ed76c3
MarjoryMel <marjorymel48@gmail.com>
2024-02-15 03:24:54 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/MarjoryMel/api/router"
+import (
+	"github.com/MarjoryMel/api/config"
+	"github.com/MarjoryMel/api/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	// Initialize router
 	router.Initialize()
