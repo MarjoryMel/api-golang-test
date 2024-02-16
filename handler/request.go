9919812f2964504c61e738dfaac6caf2fdff4dbb
MarjoryMel <marjorymel48@gmail.com>
2024-02-16 01:58:56 -0300

    feat: add update opening handler logic

--- handler/request.go
+++ handler/request.go
@@ -42,3 +42,19 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+type UpdateOpeningResponse struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningResponse) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	return fmt.Errorf("at least one valid field must be provided")
+}
